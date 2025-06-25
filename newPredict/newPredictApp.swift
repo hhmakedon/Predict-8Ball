506e9407c5d8102c685be7c7ae8fd28cc2a85499
Havee Makedon <hhmakedon@cps.edu>
2025-06-25 10:45:39 -0500

    Initial Commit

---
+++ newPredict/newPredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  newPredictApp.swift
+//  newPredict
+//
+//  Created by Havee Makedon on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct newPredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
